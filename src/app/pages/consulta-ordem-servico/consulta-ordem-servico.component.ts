4740ffbc789cf95eceb7a009f0bba999d8dc11b2
Joao Vitor Sehn <Jaovitorsehn@hotmail.com>
2024-10-28 22:44:46 -0300

    terceiro commit

--- src/app/pages/consulta-ordem-servico/consulta-ordem-servico.component.ts
+++ src/app/pages/consulta-ordem-servico/consulta-ordem-servico.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MainComponent } from "../main/main.component";
 import { MatTableModule } from '@angular/material/table';
+import { CommonModule } from '@angular/common';
 import { OrdemServico } from '../../core/models/OrdemServico';
 import { ContainerComponent } from "../../shared/container/container.component";
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { AsyncPipe } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-consulta-ordem-servico',
@@ -10,12 +23,48 @@ import { ContainerComponent } from "../../shared/container/container.component";
   imports: [
     MainComponent,
     MatTableModule,
-    ContainerComponent
+    ContainerComponent,
+    MatDatepickerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+    MatSelectModule
 ],
   templateUrl: './consulta-ordem-servico.component.html',
-  styleUrl: './consulta-ordem-servico.component.scss'
+  styleUrl: './consulta-ordem-servico.component.scss',
+  providers: [provideNativeDateAdapter()]
 })
-export class ConsultaOrdemServicoComponent {
+export class ConsultaOrdemServicoComponent implements OnInit{
+  filteredOptions!: Observable<string[]>;
+  myControl = new FormControl('');
+  selectedValue: string = "";
+  options: string[] = ['apple', 'banana', 'cherry'];
+  readonly range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
+
+  ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  opcoesData = [
+    "Data de previsão",
+    "Data de criação",
+    "Data de conclusão"
+  ]
+
   columns = [
     {
       columnDef: 'os',
@@ -67,6 +116,12 @@ export class ConsultaOrdemServicoComponent {
       descricao: 'Manutenção preventiva de máquinas',
     },
   ];
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 
   displayedColumns = this.columns.map(c => c.columnDef);
 }
